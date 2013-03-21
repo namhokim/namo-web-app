532a6c1e69edd161a09cfef862cb0521840d606e
NHKimNamo@gmail.com <NHKimNamo@gmail.com@d5d2e0aa-afaa-73b8-947c-e5cc69b6b23b>
2013-03-21 08:33:18 +0000

    upload implement +

--- drive/restlet/src/main/java/com/tistory/namocom/drive/AppEntry.java
+++ drive/restlet/src/main/java/com/tistory/namocom/drive/AppEntry.java
@@ -1,5 +1,7 @@
 package com.tistory.namocom.drive;
 
+import java.io.File;
+
 import org.restlet.Application;
 import org.restlet.Context;
 import org.restlet.Restlet;
@@ -8,6 +10,22 @@ import org.restlet.routing.Router;
 import com.tistory.namocom.drive.resource.*;
 
 public class AppEntry extends Application {
+	
+	public static final String RELEASE_PATH;
+	public final static int UPLOAD_MAX_BYTES = 52428800;	// 50MB
+	private static final String RELEASE_SUB_PATH = "\\drive\\";
+	
+	static {
+		// Release Directory Set
+		File f = new File(".");
+		String currPath = f.getAbsolutePath();
+		String rootPath = currPath.substring(0, currPath.indexOf('\\')) + RELEASE_SUB_PATH;
+		File d = new File(rootPath);
+		if(!d.exists()) d.mkdirs();
+		RELEASE_PATH = rootPath;
+		f = null;
+		d = null;
+	}
 	
 	public AppEntry() {
 		super();
@@ -19,7 +37,8 @@ public class AppEntry extends Application {
 	public Restlet createInboundRoot() {
         Router router = new Router(getContext());
   
-        router.attach("/hello", HelloResource.class);
+        router.attach("/upload", Upload.class);
+        router.attach("/download", Download.class);
         router.attachDefault(Default.class);
         
         return router;
---
+++ drive/restlet/src/main/java/com/tistory/namocom/drive/resource/Upload.java
@@ -0,0 +1,60 @@
+package com.tistory.namocom.drive.resource;
+
+import java.io.File;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.restlet.data.Status;
+import org.restlet.ext.fileupload.RestletFileUpload;
+import org.restlet.representation.Representation;
+import org.restlet.representation.StringRepresentation;
+import org.restlet.resource.Post;
+import org.restlet.resource.ServerResource;
+
+import com.tistory.namocom.drive.AppEntry;
+
+public class Upload extends ServerResource {
+
+	@Post
+	public Representation representation(Representation entity) throws Exception {
+		
+    	if (entity == null) {
+    		// POST request with no entity.
+    		setStatus(Status.CLIENT_ERROR_BAD_REQUEST);
+    		return new StringRepresentation("Uploaded Error");
+    	}
+    	
+    	// 1. Create a factory for disk-based file items
+    	DiskFileItemFactory factory = new DiskFileItemFactory();
+        factory.setSizeThreshold(AppEntry.UPLOAD_MAX_BYTES);
+        
+        // 2. Create a new file upload handler based on the Restlet
+        RestletFileUpload upload = new RestletFileUpload(factory);
+        List<FileItem> items;
+        
+        // 3. Request is parsed by the handler which generates a
+        // list of FileItems
+        items = upload.parseRequest(getRequest());
+    	
+		// Process only the uploaded item called "fileToUpload" and
+        // save it on disk
+		boolean found = false;
+		for (final Iterator<FileItem> it = items.iterator();
+				it.hasNext() && !found;) {
+			FileItem fi = it.next();
+			 if (fi.getFieldName().equals("file")) {
+				 String name = fi.getName();
+                 found = true;
+                 File file = new File(AppEntry.RELEASE_PATH + name);
+                 if(file.exists()) file.delete();
+                 fi.write(file);
+             }
+		}
+		
+		setStatus(Status.SUCCESS_CREATED);		// 201
+		return new StringRepresentation("Uploaded");
+	}
+	
+}
